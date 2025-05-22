5ec0e57182f5f2c3f43aa8dee658f3f6c282a69b
kaon-asubosi <gamepley2022@gmail.com>
2025-05-22 19:34:38 +0900

    UI関係MVP版で作成1

--- ui.py
+++ ui.py
@@ -10,14 +10,18 @@ class task_label:
         obj = task.Task(name,minutes)
         frame = tk.Frame(widgetframe,background="lightgrey",padx=10,pady=10);frame.pack()
         tk.Label(frame,text=obj.__repr__()).pack()
-side_frame = tk.Frame(root,background="lightgray",width="150")
-side_frame.pack(side="left",fill="y")
 
 footer = tk.Frame(root,background="gray",height="100",width="800")
-footer.pack(side="bottom",fill="x")
+
 
 widgetframe = tk.Frame(root)
 widgetframe.pack(fill="both")
+
+textform = tk.Entry(footer)
+formbotton = tk.Button(footer,text="Click")
+textform.pack()
+
+footer.pack(side="bottom",fill="x")
 
 task_label("料理",30)
 root.mainloop()
